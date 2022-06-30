e1303d7b5312614d48dbdf0174f4e5bdb7f184d2
elifaltundag <elif.altundag.x@gmail.com>
2022-06-30 19:19:20 +0300

    Nav background & mobile nav toggle

---
+++ main.js
@@ -0,0 +1,28 @@
+// ELEMENTS
+const btnNavToggle = document.querySelector("#btn-42-mobile");
+const navBar = document.querySelector("#nav-bar");
+const navBackground = document.querySelector("#nav-background");
+const navList = document.querySelector("#primary-nav");
+
+/* console.log(btnNavToggle); */
+console.log(navBar);
+
+btnNavToggle.addEventListener("click", () => {
+
+    const navBarDisplayed = navBar.getAttribute("displayed");
+
+    if (navBarDisplayed === "on") {
+        navBar.setAttribute("displayed", "off");
+    } else if (navBarDisplayed === "off") {
+        navBar.setAttribute("displayed", "on");
+    }
+    
+});
+
+
+const navPadding = navBar.style.padding;
+console.log(navBar.style.paddingTop);
+
+
+/* navBackground.style.height = navList.clientHeight + "px"; */
+
